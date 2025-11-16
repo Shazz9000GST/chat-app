01155486f1cbdedf81fd04878b384dac2ba4f13d
Shazz9000GST <155403421+Shazz9000GST@users.noreply.github.com>
2025-11-16 18:26:34 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_16_073202) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_16_092411) do
+  create_table "rooms", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
